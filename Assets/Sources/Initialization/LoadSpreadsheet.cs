94032f0710381cb63d6e0587b6e00825d35d5f22
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-07-01 17:18:16 +0200

    feat : added spreadsheet data and loader content : setup loader in main.unity scene chore : created spreadsheet asset bundle

---
+++ Assets/Sources/Initialization/LoadSpreadsheet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadSpreadsheet : MonoBehaviour
+{
+    private const string SheetPath = "spreadsheet_data";
+
+    private IEnumerator Start()
+    {
+        var loadOperation = Addressables.LoadAssetAsync<SpreadsheetScriptableObject>(SheetPath);
+        yield return loadOperation;
+
+        if (loadOperation.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+        {
+            SpreadsheetScriptableObject spreadSheet = loadOperation.Result;
+            Debug.LogFormat("Spreadsheet name : {0} and URL : {1}", spreadSheet.name, spreadSheet.URL);
+        }
+        else
+        {
+            Debug.LogErrorFormat("Loading Failed {0}", loadOperation.Status);
+        }
+    }
+}
---
+++ Assets/Sources/ScriptableObjects/SpreadsheetScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "spreadsheet_data", menuName = "SourceOfTruth/SpreadsheetScriptableObject")]
+public class SpreadsheetScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    private string url;
+    public string URL => url;
+}
