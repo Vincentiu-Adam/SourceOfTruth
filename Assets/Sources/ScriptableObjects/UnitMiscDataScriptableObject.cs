9bbf5149b82e3b40145f6e6001c036e195ff416f
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-07-02 12:22:41 +0200

    feat : added unit data scripts matching the spreadsheet data model

---
+++ Assets/Sources/ScriptableObjects/UnitMiscDataScriptableObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public enum UnitTribeEnum
+{
+    HUMAN,
+    BEAST,
+    MECH
+}
+
+public enum UnitRarityEnum
+{
+    COMMON,
+    UNCOMMON,
+    EPIC,
+    LEGENDARY
+}
+
+[CreateAssetMenu(fileName = "unit_misc_data", menuName = "SpreadsheetData/Units/UnitMiscDataScriptableObject")]
+public class UnitMiscDataScriptableObject : ScriptableObject
+{
+    public UnitTribeEnum Tribe;
+    public bool Fly;
+    public UnitRarityEnum Rarity;
+}
---
+++ Assets/Sources/ScriptableObjects/UnitStatsScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "unit_stat_data", menuName = "SpreadsheetData/Units/UnitStatsScriptableObject")]
+public class UnitStatsScriptableObject : ScriptableObject
+{
+    public int ID;
+
+    public string Name;
+
+    public float BaseHealth;
+    public float AttackSpeed;
+    public float AttackDamage;
+    public float Armor;
+}
