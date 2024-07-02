10fd82df8ca19fd3adc0afa7c04ac5f6986bb9fb
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-07-02 20:04:01 +0200

    feat : added UnitData and UnitRepository to store unit information

---
+++ Assets/Sources/Repository/UnitRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+public class UnitRepository
+{
+    private UnitData[] m_Units;
+    public UnitData this[int index] => m_Units[index];
+
+    public UnitRepository(int unitCount)
+    {
+        m_Units = new UnitData[unitCount];
+    }
+
+    public void AddUnit(int index, UnitData unit)
+    {
+        m_Units[index] = unit;
+    }
+
+    private IEnumerator GetEnumerator()
+    {
+        return m_Units.GetEnumerator();
+    }
+}
--- Assets/Sources/Units/UnitData.cs
+++ Assets/Sources/Units/UnitData.cs
@@ -1,6 +1,19 @@
-using UnityEngine;
-
+﻿
 public class UnitData
 {
-    
+    public float Health;
+
+    private UnitStatsScriptableObject m_UnitStats;
+    public UnitStatsScriptableObject UnitStats => m_UnitStats;
+
+    private UnitMiscDataScriptableObject m_UnitMiscStats;
+    public UnitMiscDataScriptableObject UnitMiscStats => m_UnitMiscStats;
+
+    public UnitData(UnitStatsScriptableObject unitStats, UnitMiscDataScriptableObject unitMiscStats)
+    {
+        m_UnitStats = unitStats;
+        m_UnitMiscStats = unitMiscStats;
+
+        Health = unitStats.BaseHealth;
+    }
 }
