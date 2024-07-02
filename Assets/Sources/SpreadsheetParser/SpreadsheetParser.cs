209ad407f6a75a9424c5f32b07455fe3550f9345
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-07-02 16:48:25 +0200

    feat : added data type setter methods to spreadsheet parser feat : added get value support for enum types to spreadsheet parser feat : added SpreadsheetNameAttribute to spreadsheet parser for data fields that have a different name than the spreadsheet

--- Assets/Sources/SpreadsheetParser/SpreadsheetParser.cs
+++ Assets/Sources/SpreadsheetParser/SpreadsheetParser.cs
@@ -3,10 +3,23 @@ using System.Reflection;
 
 using Newtonsoft.Json.Linq;
 
+[AttributeUsage(AttributeTargets.Field)]
+public class SpreadsheetNameAttribute : Attribute
+{
+    public string Name;
+
+    public SpreadsheetNameAttribute(string name)
+    {
+        Name = name.ToLower();
+    }
+}
+
 public class SpreadsheetColumnData
 {
     public int[] ColumnIndices;
     public string[] ColumnNames;
+
+    public int Length => ColumnIndices.Length;
 
     public SpreadsheetColumnData(int columnSize)
     {
@@ -17,7 +30,7 @@ public class SpreadsheetColumnData
 
 public class SpreadsheetParser
 {
-    public SpreadsheetColumnData CreateColumnDataForType<T>(JArray spreadsheetColumns) where T : SpreadsheetDataScriptableObject
+    public static SpreadsheetColumnData CreateColumnDataForType<T>(JArray spreadsheetColumns) where T : SpreadsheetDataScriptableObject
     {
         //create temp data with all the present columns in the spreadsheet (these are not all relevant and can contain empty data), so we can filter which ones we need based on type
         SpreadsheetColumnData tempColumnData = new SpreadsheetColumnData(spreadsheetColumns.Count);
@@ -25,21 +38,21 @@ public class SpreadsheetParser
         int columnDataIndex = 0;
         for (int i = 0; i < spreadsheetColumns.Count; i++)
         {
-            string columnName = GetValue(spreadsheetColumns[i]);
-            if (string.IsNullOrEmpty(columnName))
+            object columnName = GetValue(spreadsheetColumns[i]);
+            if (columnName == null)
             {
                 continue; //skip empty columns
             }
 
             //check if column name is among type fields (only use public fields for now)
             Type dataType = typeof(T);
-            if (!ContainsField(columnName, dataType))
+            if (!ContainsField(columnName.ToString(), dataType, out _))
             {
                 continue; //skip if field does not exist
             }
 
             tempColumnData.ColumnIndices[columnDataIndex] = i;
-            tempColumnData.ColumnNames[columnDataIndex++] = columnName;
+            tempColumnData.ColumnNames[columnDataIndex++] = columnName.ToString();
         }
 
         //create new column data with only relevant size and init the data
@@ -53,7 +66,27 @@ public class SpreadsheetParser
         return columnData;
     }
 
-    private string GetValue(JToken userValue)
+    //fetch the data based on the column data from the spreadsheet and set it to the data container
+    public static void SetDataForTypeFromColumn<T>(JArray spreadsheetColumns, SpreadsheetColumnData columnData, ref T dataContainer) where T : SpreadsheetDataScriptableObject
+    {
+        Type dataType = typeof(T);
+        for (int i = 0; i < columnData.Length; i++)
+        {
+            int columnIndex = columnData.ColumnIndices[i];
+            string columnName = columnData.ColumnNames[i];
+
+            //set the value to the field 
+            if (ContainsField(columnName, dataType, out FieldInfo field))
+            {
+                JToken userValue = spreadsheetColumns[columnIndex];
+                object columnValue = field.FieldType.IsEnum ? GetEnumValue(userValue, field.FieldType) : GetValue(userValue);
+
+                field.SetValue(dataContainer, columnValue);
+            }
+        }
+    }
+
+    private static object GetValue(JToken userValue)
     {
         userValue = userValue["userEnteredValue"];
         if (userValue == null)
@@ -65,27 +98,69 @@ public class SpreadsheetParser
         JToken value = userValue["stringValue"];
         if (value != null)
         {
-            return value.ToString();
+            return value.Value<string>();
         }
 
         value = userValue["numberValue"];
         if (value != null)
         {
-            return value.ToString();
+            return value.Value<float>();
         }
 
         value = userValue["boolValue"];
-        return value?.ToString();
+        return value?.Value<bool>();
+    }
+
+    private static object GetEnumValue(JToken userValue, Type enumType)
+    {
+        userValue = userValue["userEnteredValue"];
+        if (userValue == null)
+        {
+            return null;
+        }
+
+        //enums will always be strings in the spreadsheet (otherwise wtf are you using numbers for enums? :/)
+        JToken value = userValue["stringValue"];
+        if (value == null)
+        {
+            return null;
+        }
+
+        string lowerCaseValue = value.Value<string>().ToLower();
+
+        Array enumValues = Enum.GetValues(enumType);
+        foreach (object enumValue in enumValues)
+        {
+            string lowerCaseEnumValue = enumValue.ToString().ToLower();
+            if (lowerCaseValue.Equals(lowerCaseEnumValue))
+            {
+                return enumValue;
+            }
+        }
+
+        return null;
     }
 
     //check if dataType contains field specified by fieldName. We want to completely ignore case so do the check with lowercase
-    private bool ContainsField(string fieldName, Type dataType)
+    private static bool ContainsField(string fieldName, Type dataType, out FieldInfo foundField)
     {
+        foundField = null;
+
         //only look for public fields
         foreach (FieldInfo field in dataType.GetFields())
         {
-            if (fieldName.ToLower().Equals(field.Name.ToLower()))
+            string publicFieldName = field.Name.ToLower();
+
+            //if we have a custom spreadsheet name attribute use that instead
+            var fieldNameAttribute = field.GetCustomAttribute<SpreadsheetNameAttribute>();
+            if (fieldNameAttribute != null)
             {
+                publicFieldName = fieldNameAttribute.Name;
+            }
+
+            if (fieldName.ToLower().Equals(publicFieldName))
+            {
+                foundField = field;
                 return true;
             }
         }
