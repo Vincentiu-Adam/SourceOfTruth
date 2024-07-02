78dca96678429dd1d9433aa07b9f7aa23c74db76
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-07-02 16:50:10 +0200

    chore : cleaned up formatting errors and tweaked editorconfig file

--- Assets/Sources/ScriptableObjects/UnitMiscDataScriptableObject.cs
+++ Assets/Sources/ScriptableObjects/UnitMiscDataScriptableObject.cs
@@ -16,7 +16,7 @@ public enum UnitRarityEnum
 }
 
 //using an abstract class just to inherit from ScriptableObject
-public abstract class SpreadsheetDataScriptableObject : ScriptableObject {};
+public abstract class SpreadsheetDataScriptableObject : ScriptableObject { };
 
 [CreateAssetMenu(fileName = "unit_misc_data", menuName = "SpreadsheetData/Units/UnitMiscDataScriptableObject")]
 public class UnitMiscDataScriptableObject : SpreadsheetDataScriptableObject
