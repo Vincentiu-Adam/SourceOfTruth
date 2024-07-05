791c40d93bcea542cf0abfc4cf48c37ae3648860
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-07-05 18:57:00 +0200

    content : setup more character prefabs

--- Assets/Sources/ScriptableObjects/UnitVisualDataScriptableObject.cs
+++ Assets/Sources/ScriptableObjects/UnitVisualDataScriptableObject.cs
@@ -3,9 +3,21 @@
 public enum UnitVisualEnum
 {
     MECH,
+    MECH_FIGHTER_A,
+    MECH_FIGHTER_B,
+    MECH_FIGHTER_C,
     WIZARD_A,
     WIZARD_B,
-    BEAST_A
+    BEAST_A,
+    MONSTER_A,
+    MONSTER_B,
+    MONSTER_C,
+    MONSTER_D,
+    MONSTER_E,
+    MONSTER_F,
+    KNIGHT,
+    BANDITOS_A,
+    BANDITOS_B
 }
 
 public enum UnitVFXEnum
